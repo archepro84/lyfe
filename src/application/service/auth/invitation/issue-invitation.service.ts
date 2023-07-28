077331e3f7971fa9363f138ede53b18049e7186e
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-07-29 01:15:59 +0900

    refactor: Refactoring invitation code generation logic in issueInvitationService

--- src/application/service/auth/invitation/issue-invitation.service.ts
+++ src/application/service/auth/invitation/issue-invitation.service.ts
@@ -2,14 +2,12 @@ import { IssueInvitationUsecase } from '@application/port/in/auth/invitation/iss
 import { IssueInvitationCommand } from '@application/port/in/auth/invitation/command/issue-invitation.command';
 import { Invitation } from '@domain/auth/invitation';
 import { InvitationRepository } from '@application/port/out/auth/invitation/invitation.repository';
-import { BcryptPort } from '@application/port/security/bcrypt/bcrypt.port';
 import { LoggerPort } from '@application/port/common/logger/logger.port';
 
 export class IssueInvitationService implements IssueInvitationUsecase {
   constructor(
     private readonly logger: LoggerPort,
     private readonly invitationRepository: InvitationRepository,
-    private readonly bcryptPort: BcryptPort,
   ) {}
 
   async exec(
@@ -18,21 +16,23 @@ export class IssueInvitationService implements IssueInvitationUsecase {
     const invitationCode = this.createInvitationCode();
     this.logger.debug(this.constructor.name, invitationCode);
 
-    const hashedInvitationCode = await this.bcryptPort.hash(invitationCode);
-
     return await this.invitationRepository.issueInvitation(
       new Invitation(
         issueInvitationCommand.invitationType,
         issueInvitationCommand.inviterId,
-        hashedInvitationCode,
+        invitationCode,
         issueInvitationCommand.inviteePhoneNumber,
       ),
     );
   }
 
-  // 8자리의 숫자로 구성된 InvitationCode를 반환한다.
+  // 숫자, 영문로 이루어진 6자리 문자열 생성
   createInvitationCode(): string {
-    const invitationCode = Math.floor(Math.random() * 89999999) + 10000000;
-    return invitationCode.toString();
+    const invitationCodeLength = 6;
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    return Array.from({ length: invitationCodeLength })
+      .map(() => charset.charAt(Math.floor(Math.random() * charset.length)))
+      .join('');
   }
 }
