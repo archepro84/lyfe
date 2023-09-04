56c3b5876d56ca705fa4953c999684aa6c67b9b3
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-09-04 10:32:15 +0900

    feat(terraform): Add template provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,9 +6,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 1.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+# https://registry.terraform.io/providers/hashicorp/template/2.2.0
+provider "template" {
 }
