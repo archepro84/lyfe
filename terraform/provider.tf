01890d0d17ee74bc59adf7cf624c5f2f0cf8441b
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-09-04 11:14:20 +0900

    feat(terraform): Remove template provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,17 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 1.0"
-    }
   }
 }
 
 provider "aws" {
   region = var.region
-}
-
-# https://registry.terraform.io/providers/hashicorp/template/2.2.0
-provider "template" {
 }
