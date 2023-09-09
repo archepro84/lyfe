f361c43694e703854245de7513fa478bcd3ba9ec
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-09-09 21:55:09 +0900

    feat: Add Custom transactional decorator with transaction manager for mongoose sessions

---
+++ src/common/decorator/transactional.decorator.ts
@@ -0,0 +1,39 @@
+import { TransactionManager } from '@adapter/out/persistence/common/transaction/transaction.manager';
+import { transactionSessionStorage } from '@adapter/out/persistence/common/transaction/transaction.session.storage';
+
+export function Transactional() {
+  return function (
+    target: any,
+    propertyName: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]) {
+      const transactionManager = TransactionManager.getInstance();
+      const connection = transactionManager.getConnection();
+
+      if (transactionSessionStorage.getTransaction()) {
+        await originalMethod.apply(this, args);
+      }
+
+      const session = await connection.startSession();
+      const session2 = await connection.startSession();
+      transactionSessionStorage.setTransaction(session);
+      await session2.endSession();
+
+      try {
+        session.startTransaction();
+        await originalMethod.apply(this, args);
+        await session.commitTransaction();
+      } catch (err) {
+        await session.abortTransaction();
+        throw err;
+      } finally {
+        await session.endSession();
+      }
+    };
+
+    return descriptor;
+  };
+}
