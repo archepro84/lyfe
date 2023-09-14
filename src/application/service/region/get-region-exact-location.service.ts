637de665dc5b88acb81fe5865b5e7ca13242a2dd
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-09-15 02:06:07 +0900

    fix: Fix get region nullable exception

--- src/application/service/region/get-region-exact-location.service.ts
+++ src/application/service/region/get-region-exact-location.service.ts
@@ -2,6 +2,7 @@ import { RegionRepository } from '@application/port/out/region/region.repository
 import { GetRegionExactLocationCommand } from '@application/port/in/region/region.command';
 import { Region } from '@domain/region/region';
 import { GetRegionExactLocationQuery } from '@application/port/in/region/get-region-exact-location.query';
+import { NotFoundException } from '@common/exception/not-found.exception';
 
 export class GetRegionExactLocationService
   implements GetRegionExactLocationQuery
@@ -11,6 +12,11 @@ export class GetRegionExactLocationService
   async exec(
     getRegionExactLocationCommand: GetRegionExactLocationCommand,
   ): Promise<Region> {
-    return await this.regionRepository.find(getRegionExactLocationCommand);
+    const region = await this.regionRepository.find(
+      getRegionExactLocationCommand,
+    );
+    if (!region)
+      throw new NotFoundException('해당하는 지역이 존재하지 않습니다.');
+    return region;
   }
 }
