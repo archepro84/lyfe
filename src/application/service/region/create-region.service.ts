f65ef33d4eb33572140b77a7c0fc0e05ad964513
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-09-21 13:28:09 +0900

    refactor: Change return type to Region and throw error if Region exists in create region service

--- src/application/service/region/create-region.service.ts
+++ src/application/service/region/create-region.service.ts
@@ -2,11 +2,18 @@ import { CreateRegionUsecase } from '@application/port/in/region/create-region.u
 import { RegionRepository } from '@application/port/out/region/region.repository';
 import { CreateRegionCommand } from '@application/port/in/region/region.command';
 import { Region } from '@domain/region/region';
+import { AlreadyExistsException } from '@common/exception/already-exists.exception';
+import { Transactional } from '@common/decorator/transactional.decorator';
 
 export class CreateRegionService implements CreateRegionUsecase {
   constructor(private readonly regionRepository: RegionRepository) {}
 
-  async exec(createRegionCommand: CreateRegionCommand): Promise<void> {
+  @Transactional()
+  async exec(createRegionCommand: CreateRegionCommand): Promise<Region> {
+    const region = await this.regionRepository.find(createRegionCommand);
+    if (region)
+      throw new AlreadyExistsException('해당 지역이 이미 존재합니다.');
+
     await this.regionRepository.insert(
       Region.newInstanceWithDetails(
         createRegionCommand.city,
@@ -14,5 +21,10 @@ export class CreateRegionService implements CreateRegionUsecase {
         createRegionCommand.neighborhood,
       ),
     );
+
+    const newRegion = await this.regionRepository.find(createRegionCommand);
+    newRegion.checkLoaded();
+
+    return newRegion;
   }
 }
