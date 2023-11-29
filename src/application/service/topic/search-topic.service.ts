4f4e568cac9c4b4566632835078affc288a345df
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-11-29 22:31:03 +0900

    feat: Add topic cursor search

--- src/application/service/topic/search-topic.service.ts
+++ src/application/service/topic/search-topic.service.ts
@@ -1,14 +1,20 @@
 import { TopicRepository } from '@application/port/out/topic/topic.repository';
 import { FindAllTopicUsecase } from '@application/port/in/topic/find-all-topic.usecase';
-import { FindAllTopicQuery } from '@application/port/in/topic/query/find-all-topic.query';
-import { Paginated } from '@application/port/out/repository.port';
+import {
+  Paginated,
+  PaginatedQueryParams,
+} from '@application/port/out/repository.port';
 import { Topic } from '@domain/topic/topic';
 
 export class SearchTopicService implements FindAllTopicUsecase {
   constructor(private readonly topicRepository: TopicRepository) {}
 
-  async exec(query: FindAllTopicQuery): Promise<Paginated<Topic>> {
-    const topics = await this.topicRepository.searchTopic(query);
+  async exec(query: PaginatedQueryParams): Promise<Paginated<Topic>> {
+    let topics: Paginated<Topic>;
+
+    if (query.cursor)
+      topics = await this.topicRepository.findCursorPaginated(query);
+    else topics = await this.topicRepository.findManyPaginated(query);
 
     return topics;
   }
