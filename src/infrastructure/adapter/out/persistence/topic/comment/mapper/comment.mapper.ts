9a44f8d22e304655157f2cddbd0d80fe4c35c609
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-12-09 12:16:28 +0900

    feat: Add comment mapper with mongoose schema

---
+++ src/infrastructure/adapter/out/persistence/topic/comment/mapper/comment.mapper.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { MapperPort } from '@application/port/out/mapper.port';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { RegionFactory } from '@domain/region/region';
+import { Geometry } from '@domain/user/geometry';
+import { CommentEntity } from '@infrastructure/adapter/out/persistence/topic/comment/schema/comment.schema';
+import { Comment, CommentFactory } from '@domain/topic/comment/comment';
+import { TopicUser } from '@domain/topic/topic-user';
+
+@Injectable()
+export class CommentMapper implements MapperPort<CommentEntity, Comment> {
+  constructor(
+    @InjectModel(Comment.name) private readonly model: Model<CommentEntity>,
+  ) {}
+
+  public toDomain(commentEntity: CommentEntity): Comment {
+    if (!commentEntity) return null;
+
+    return CommentFactory.newInstance({
+      id: commentEntity._id,
+      topicId: commentEntity.topicId,
+      parentId: commentEntity.patentId ?? undefined,
+      content: commentEntity.content,
+      user: new TopicUser({
+        id: commentEntity.user._id,
+        nickname: commentEntity.user.nickname,
+      }),
+      createdAt: commentEntity.createdAt,
+      updatedAt: commentEntity.updatedAt,
+      deletedAt: commentEntity.deletedAt,
+    });
+  }
+
+  public toDomains(commentEntities: CommentEntity[]): Comment[] {
+    return commentEntities.map(this.toDomain);
+  }
+
+  public toPersistence(comment: Comment): CommentEntity {
+    return new this.model({
+      id: comment.id,
+      topicId: comment.topicId,
+      patentId: comment.parentId,
+      content: comment.content,
+      user: {
+        _id: comment.user.id,
+        nickname: comment.user.nickname,
+      },
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt,
+      deletedAt: comment.deletedAt,
+    });
+  }
+}
---
+++ src/infrastructure/adapter/out/persistence/topic/comment/schema/comment.schema.ts
@@ -0,0 +1,84 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Theme } from '@domain/topic/topic';
+import { ImageEntity } from '@infrastructure/adapter/out/persistence/topic/schema/image.schema';
+import { Document } from 'mongoose';
+import { GeometryEntity } from '@infrastructure/adapter/out/persistence/user/schema/geometry.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {
+  VoteEntity,
+  VoteMongoSchema,
+  VoteSchema,
+} from '@infrastructure/adapter/out/persistence/topic/schema/vote.schema';
+import { TopicUserEntity } from '@infrastructure/adapter/out/persistence/topic/schema/topic-user.schema';
+
+export class CommentEntity extends Document {
+  _id?: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    description: '게시글 id',
+    example: '649ce793f331996dcc3cddab',
+  })
+  @IsString()
+  topicId: string;
+
+  @ApiProperty({
+    required: true,
+    type: () => TopicUserEntity,
+    description: '게시글 작성자정보',
+  })
+  user: TopicUserEntity;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    description: '댓글 내용',
+    example: '댓글 내용 입니다.',
+  })
+  @IsString()
+  content: string;
+
+  @ApiProperty({
+    required: true,
+    type: 'string',
+    description: '부모 댓글 id',
+    example: '649ce793f331996dcc3cddab',
+  })
+  @IsOptional()
+  @IsString()
+  patentId?: string;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+
+  deletedAt?: Date;
+}
+
+@Schema({ collection: 'comments', _id: true })
+export class CommentMongoSchema {
+  @Prop({ required: true, type: 'string' })
+  topicId: string;
+
+  @Prop({ required: true })
+  user: TopicUserEntity;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: false })
+  patent?: string;
+
+  @Prop({ required: true, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ required: true, default: Date.now })
+  updatedAt: Date;
+
+  @Prop({ select: false })
+  deletedAt?: Date;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(CommentMongoSchema);
